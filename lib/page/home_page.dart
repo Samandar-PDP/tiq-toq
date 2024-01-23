676a4c165ccd2d8111217cc3b7297b6c5d3bf565
samandar-me <samandarcoderdev@gmail.com>
2024-01-23 15:29:08 +0500

    fjdklsa

--- lib/page/home_page.dart
+++ lib/page/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lottie/lottie.dart';
 import 'package:tiktok_reels/repository/video_repository.dart';
 import 'package:video_player/video_player.dart';
 
@@ -14,6 +16,8 @@ class HomePage extends ConsumerStatefulWidget {
 
 class _HomePage extends ConsumerState<HomePage> {
   late VideoPlayerController _controller;
+  bool _isClicked = false;
+  bool _showAnim = false;
 
   @override
   void initState() {
@@ -31,9 +35,7 @@ class _HomePage extends ConsumerState<HomePage> {
   void _setVideo(String link) {
     _controller = VideoPlayerController.networkUrl(Uri.parse(link))
       ..initialize().then((value) {
-        setState(() {
-
-        });
+        setState(() {});
       });
     _controller.play();
   }
@@ -53,19 +55,77 @@ class _HomePage extends ConsumerState<HomePage> {
 
   Widget _successField(List<Videos> videos) {
     return SafeArea(
-      child: PageView.builder(
-        scrollDirection: Axis.vertical,
-        itemCount: videos.length,
-        onPageChanged: (index) {
-          _setVideo(videos[0].videoFiles?[index].link ?? "");
-        },
-        itemBuilder: (context, index) {
-          return AspectRatio(
-            aspectRatio: 9 / 16,
-            child: VideoPlayer(_controller),
-          );
-        },
-      ),
+        child: GestureDetector(
+          onDoubleTap: () async {
+            setState(() {
+              _showAnim = true;
+            });
+            await Future.delayed(const Duration(seconds: 1));
+            setState(() {
+              _showAnim = false;
+            });
+          },
+          child: Stack(
+            children: [
+              PageView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: videos.length,
+                onPageChanged: (index) {
+                  _setVideo(videos[index].videoFiles?[3].link ?? "");
+                },
+                itemBuilder: (context, index) {
+                  return AspectRatio(
+                    aspectRatio: 9 / 16,
+                    child: VideoPlayer(_controller),
+                  );
+                },
+              ),
+              Positioned(
+                bottom: 40,
+                right: 10,
+                child: Column(
+                  children: [
+                    IconButton(onPressed: () {
+                      setState(() {
+                        _isClicked = !_isClicked;
+                      });
+                    }, icon: Icon(
+                      _isClicked ? CupertinoIcons.heart_fill : CupertinoIcons.heart,
+                      color: _isClicked ? Colors.red : Colors.white,
+                    )),
+                    Text("324323"),
+                    const SizedBox(height: 10),
+                    IconButton(onPressed: () {
+                      _showComment();
+                    }, icon: const Icon(CupertinoIcons.chat_bubble_text, color: Colors.white,)),
+                    Text("4566"),
+                    const SizedBox(height: 10),
+                    IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.paperplane, color: Colors.white,)),
+                    Text("12"),
+                    const SizedBox(height: 10),
+                    IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.ellipsis_vertical, color: Colors.white,)),
+                    const SizedBox(height: 10),
+                  ],
+                ),
+              ),
+              if(_showAnim)
+                Center(
+                  child: Lottie.asset('assets/json/like.json'),
+                )
+            ],
+          ),
+        )
     );
+  }
+  _showComment() {
+    showModalBottomSheet(context: context, builder: (context) => Padding(
+      padding: EdgeInsets.all(20),
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Divider()
+        ],
+      ),
+    ));
   }
 }
